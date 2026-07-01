bfddba70dbfcdf5194f753f707cfacb3d9a499ff
Sahil Bishnoi <bishnoisahil29736@gmail.com>
2026-07-01 15:13:59 +0530

    Fix fields syntax in SprintSerializer and TaskSerializer

--- sprint/serializers.py
+++ sprint/serializers.py
@@ -1,14 +1,17 @@
+
+# pyrefly: ignore [missing-import]
 from rest_framework import serializers
+# pyrefly: ignore [missing-import]
 from .models import Sprint, Task
 
 
 class SprintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sprint
-        fields = '_all_'
+        fields = '__all__'
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '_all_'
+        fields = '__all__'
